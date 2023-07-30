0b3cad4bea66275f6726c3ebd4899930ceda6438
ade_lana <lanadvictor@yahoo.co.uk>
2023-07-30 02:00:44 +0100

    client: extend PasswordInput types

--- client/src/components/PasswordInput/index.tsx
+++ client/src/components/PasswordInput/index.tsx
@@ -5,43 +5,66 @@ import type { UseFormRegisterReturn, FieldError } from "react-hook-form";
 
 import PasswordAdornment from "./PasswordAdornment";
 
-type Name = "password" | "confirmPassword";
+type Name =
+  | "password"
+  | "confirmPassword"
+  | "currentPassword"
+  | "newPassword"
+  | "confirmNewPassword";
+
 type Password<TName extends Name> = UseFormRegisterReturn<TName>;
 
-type PasswordProps = TextFieldProps & {
+interface PasswordProps {
   id: "password";
   label: "Password";
   register: Password<"password">;
-  fieldError: FieldError | undefined;
-};
+}
 
-type ConfirmPasswordProps = TextFieldProps & {
+interface ConfirmPasswordProps {
   id: "confirm-password";
   label: "Confirm Password";
   register: Password<"confirmPassword">;
-  fieldError: FieldError | undefined;
-};
+}
 
-type PasswordFieldProps = PasswordProps | ConfirmPasswordProps;
+interface CurrentPasswordProps {
+  id: "current-password";
+  label: "Current Password";
+  register: Password<"currentPassword">;
+}
 
-const PasswordInput = ({
-  id,
-  label,
-  register,
-  fieldError,
-  ...props
-}: PasswordFieldProps) => {
+interface NewPasswordProps {
+  id: "new-password";
+  label: "New Password";
+  register: Password<"newPassword">;
+}
+
+interface ConfirmNewPasswordProps {
+  id: "confirm-new-password";
+  label: "Confirm New Password";
+  register: Password<"confirmNewPassword">;
+}
+
+type PasswordInputProps = TextFieldProps &
+  (
+    | PasswordProps
+    | ConfirmPasswordProps
+    | CurrentPasswordProps
+    | NewPasswordProps
+    | ConfirmNewPasswordProps
+  ) & { fieldError: FieldError | undefined };
+
+const PasswordInput = (props: PasswordInputProps) => {
+  const { id, label, register, fieldError, ...muiProps } = props;
+
   const [isVisible, setIsVisible] = React.useState(false);
 
-  const handleClick = () => {
-    setIsVisible(!isVisible);
-  };
+  const handleClick = () => setIsVisible(!isVisible);
 
   const ariaId = fieldError ? `${id}-error-message` : undefined;
 
   return (
     <TextField
-      {...props}
+      {...muiProps}
       {...register}
       id={id}
       label={label}
