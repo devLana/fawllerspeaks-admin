ac01a11765445369439ed214907b639f7e350019
ade_lana <lanadvictor@yahoo.co.uk>
2023-08-12 10:43:28 +0100

    client: View profile page

--- client/src/pages/settings/me/index.tsx
+++ client/src/pages/settings/me/index.tsx
@@ -1,8 +1,54 @@
+import { gql, useApolloClient } from "@apollo/client";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+import { useSession } from "@context/SessionContext";
+import UserAvatar from "@components/UserAvatar";
 import settingsLayout from "@utils/settings/settingsLayout";
+import NextLink from "@components/NextLink";
 import { type NextPageWithLayout } from "@types";
 
+interface User {
+  email: string;
+  image: string;
+  firstName: string;
+  lastName: string;
+}
+
 const Me: NextPageWithLayout = () => {
-  return <div />;
+  const client = useApolloClient();
+
+  const { userId } = useSession();
+  const user = client.readFragment<User>({
+    id: userId ?? "",
+    fragment: gql`
+      fragment GetUserProfile on User {
+        image
+        firstName
+        lastName
+      }
+    `,
+  });
+
+  return (
+    <>
+      <Box>
+        <Stack direction="row" spacing={4} alignItems="center">
+          <UserAvatar
+            sx={{ width: { xs: 100, md: 150 }, height: { xs: 100, md: 150 } }}
+            user={user}
+          />
+          <Stack>
+            <Typography gutterBottom sx={{ wordBreak: "break-word" }}>
+              {user?.firstName ?? "Unknown"} {user?.lastName ?? "User"}
+            </Typography>
+            <NextLink href="/settings/me/edit">Edit Profile</NextLink>
+          </Stack>
+        </Stack>
+      </Box>
+    </>
+  );
 };
 
 Me.layout = settingsLayout("My profile", { title: "Admin Profile" });
