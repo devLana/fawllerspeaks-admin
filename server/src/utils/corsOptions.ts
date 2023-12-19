243c9c1f53703a3e3085b9f12abf6550fae89c48
ade_lana <lanadvictor@yahoo.co.uk>
2023-12-19 19:44:02 +0100

    [server]: add localhost:3000 to list of allowed origins

--- server/src/utils/corsOptions.ts
+++ server/src/utils/corsOptions.ts
@@ -12,6 +12,7 @@ export const corsOptions: CorsOptions = {
       : [
           "http://localhost:4000",
           "http://localhost:4040",
+          "http://localhost:3000",
           "https://sandbox.embed.apollographql.com",
         ],
   methods: "GET,POST",
