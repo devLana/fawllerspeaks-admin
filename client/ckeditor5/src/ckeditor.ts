7a316578af5fe8d346a5aecf46d981b69989786d
ade_lana <lanadvictor@yahoo.co.uk>
2024-04-29 04:05:01 +0100

    [client]: reconfigure and rebuild ckeditor

--- client/ckeditor5/src/ckeditor.ts
+++ client/ckeditor5/src/ckeditor.ts
@@ -3,214 +3,266 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
 
-import { Alignment } from '@ckeditor/ckeditor5-alignment';
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import { Alignment } from "@ckeditor/ckeditor5-alignment";
+import { Autoformat } from "@ckeditor/ckeditor5-autoformat";
 import {
-	Bold,
-	Italic,
-	Strikethrough,
-	Subscript,
-	Superscript,
-	Underline
-} from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
-import type { EditorConfig } from '@ckeditor/ckeditor5-core';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
-import { FontBackgroundColor, FontColor, FontSize } from '@ckeditor/ckeditor5-font';
-import { Heading, Title } from '@ckeditor/ckeditor5-heading';
-import { Highlight } from '@ckeditor/ckeditor5-highlight';
-import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
-import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
-import { DataFilter, DataSchema, GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
+  Bold,
+  Italic,
+  Strikethrough,
+  Subscript,
+  Superscript,
+  Underline,
+} from "@ckeditor/ckeditor5-basic-styles";
+import { BlockQuote } from "@ckeditor/ckeditor5-block-quote";
+import type { EditorConfig } from "@ckeditor/ckeditor5-core";
+import { Essentials } from "@ckeditor/ckeditor5-essentials";
+import { FindAndReplace } from "@ckeditor/ckeditor5-find-and-replace";
 import {
-	AutoImage,
-	Image,
-	ImageCaption,
-	ImageInsert,
-	ImageResize,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload
-} from '@ckeditor/ckeditor5-image';
-import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
-import { AutoLink, Link, LinkImage } from '@ckeditor/ckeditor5-link';
-import { List, ListProperties, TodoList } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
-import { PageBreak } from '@ckeditor/ckeditor5-page-break';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
-import { SelectAll } from '@ckeditor/ckeditor5-select-all';
-import { ShowBlocks } from '@ckeditor/ckeditor5-show-blocks';
-import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
+  FontBackgroundColor,
+  FontColor,
+  FontSize,
+} from "@ckeditor/ckeditor5-font";
+import { Heading } from "@ckeditor/ckeditor5-heading";
+import { HorizontalLine } from "@ckeditor/ckeditor5-horizontal-line";
 import {
-	SpecialCharacters,
-	SpecialCharactersArrows,
-	SpecialCharactersCurrency,
-	SpecialCharactersEssentials,
-	SpecialCharactersLatin,
-	SpecialCharactersMathematical,
-	SpecialCharactersText
-} from '@ckeditor/ckeditor5-special-characters';
+  AutoImage,
+  Image,
+  ImageCaption,
+  ImageInsert,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+} from "@ckeditor/ckeditor5-image";
+import { Indent, IndentBlock } from "@ckeditor/ckeditor5-indent";
+import { AutoLink, Link, LinkImage } from "@ckeditor/ckeditor5-link";
+import { List, ListProperties, TodoList } from "@ckeditor/ckeditor5-list";
+import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
+import { PasteFromOffice } from "@ckeditor/ckeditor5-paste-from-office";
+import { RemoveFormat } from "@ckeditor/ckeditor5-remove-format";
+import { SelectAll } from "@ckeditor/ckeditor5-select-all";
+import { ShowBlocks } from "@ckeditor/ckeditor5-show-blocks";
 import {
-	Table,
-	TableCaption,
-	TableCellProperties,
-	TableColumnResize,
-	TableProperties,
-	TableToolbar
-} from '@ckeditor/ckeditor5-table';
-import { TextTransformation } from '@ckeditor/ckeditor5-typing';
-import { AccessibilityHelp } from '@ckeditor/ckeditor5-ui';
-import { Undo } from '@ckeditor/ckeditor5-undo';
-import { SimpleUploadAdapter } from '@ckeditor/ckeditor5-upload';
-import { WordCount } from '@ckeditor/ckeditor5-word-count';
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersLatin,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+} from "@ckeditor/ckeditor5-special-characters";
+import {
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+} from "@ckeditor/ckeditor5-table";
+import { TextTransformation } from "@ckeditor/ckeditor5-typing";
+import { AccessibilityHelp } from "@ckeditor/ckeditor5-ui";
+import { Undo } from "@ckeditor/ckeditor5-undo";
+import { SimpleUploadAdapter } from "@ckeditor/ckeditor5-upload";
 
 // You can read more about extending the build with additional plugins in the "Installing plugins" guide.
 // See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
 
 class Editor extends ClassicEditor {
-	public static override builtinPlugins = [
-		AccessibilityHelp,
-		Alignment,
-		AutoImage,
-		AutoLink,
-		Autoformat,
-		BlockQuote,
-		Bold,
-		DataFilter,
-		DataSchema,
-		Essentials,
-		FindAndReplace,
-		FontBackgroundColor,
-		FontColor,
-		FontSize,
-		GeneralHtmlSupport,
-		Heading,
-		Highlight,
-		HorizontalLine,
-		HtmlEmbed,
-		Image,
-		ImageCaption,
-		ImageInsert,
-		ImageResize,
-		ImageStyle,
-		ImageToolbar,
-		ImageUpload,
-		Indent,
-		IndentBlock,
-		Italic,
-		Link,
-		LinkImage,
-		List,
-		ListProperties,
-		MediaEmbed,
-		MediaEmbedToolbar,
-		PageBreak,
-		Paragraph,
-		PasteFromOffice,
-		RemoveFormat,
-		SelectAll,
-		ShowBlocks,
-		SimpleUploadAdapter,
-		SourceEditing,
-		SpecialCharacters,
-		SpecialCharactersArrows,
-		SpecialCharactersCurrency,
-		SpecialCharactersEssentials,
-		SpecialCharactersLatin,
-		SpecialCharactersMathematical,
-		SpecialCharactersText,
-		Strikethrough,
-		Subscript,
-		Superscript,
-		Table,
-		TableCaption,
-		TableCellProperties,
-		TableColumnResize,
-		TableProperties,
-		TableToolbar,
-		TextTransformation,
-		Title,
-		TodoList,
-		Underline,
-		Undo,
-		WordCount
-	];
+  public static override builtinPlugins = [
+    AccessibilityHelp,
+    Alignment,
+    AutoImage,
+    AutoLink,
+    Autoformat,
+    BlockQuote,
+    Bold,
+    Essentials,
+    FindAndReplace,
+    FontBackgroundColor,
+    FontColor,
+    FontSize,
+    Heading,
+    HorizontalLine,
+    Image,
+    ImageCaption,
+    ImageInsert,
+    ImageResize,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    LinkImage,
+    List,
+    ListProperties,
+    Paragraph,
+    PasteFromOffice,
+    RemoveFormat,
+    SelectAll,
+    ShowBlocks,
+    SimpleUploadAdapter,
+    SpecialCharacters,
+    SpecialCharactersArrows,
+    SpecialCharactersCurrency,
+    SpecialCharactersEssentials,
+    SpecialCharactersLatin,
+    SpecialCharactersMathematical,
+    SpecialCharactersText,
+    Strikethrough,
+    Subscript,
+    Superscript,
+    Table,
+    TableCaption,
+    TableCellProperties,
+    TableColumnResize,
+    TableProperties,
+    TableToolbar,
+    TextTransformation,
+    TodoList,
+    Underline,
+    Undo,
+  ];
 
-	public static override defaultConfig: EditorConfig = {
-		toolbar: {
-			items: [
-				'accessibilityHelp',
-				'|',
-				'undo',
-				'redo',
-				'|',
-				'heading',
-				'|',
-				'fontSize',
-				'fontColor',
-				'fontBackgroundColor',
-				'|',
-				'bold',
-				'italic',
-				'underline',
-				'strikethrough',
-				'subscript',
-				'superscript',
-				'highlight',
-				'|',
-				'outdent',
-				'indent',
-				'alignment',
-				'|',
-				'link',
-				'imageInsert',
-				'mediaEmbed',
-				'|',
-				'blockQuote',
-				'insertTable',
-				'numberedList',
-				'bulletedList',
-				'todoList',
-				'horizontalLine',
-				'specialCharacters',
-				'|',
-				'htmlEmbed',
-				'sourceEditing',
-				'|',
-				'findAndReplace',
-				'selectAll',
-				'showBlocks',
-				'pageBreak',
-				'|',
-				'removeFormat'
-			]
-		},
-		language: 'en',
-		image: {
-			toolbar: [
-				'imageTextAlternative',
-				'toggleImageCaption',
-				'imageStyle:inline',
-				'imageStyle:block',
-				'imageStyle:side',
-				'linkImage'
-			]
-		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells',
-				'tableCellProperties',
-				'tableProperties'
-			]
-		}
-	};
+  public static override defaultConfig: EditorConfig = {
+    toolbar: {
+      items: [
+        "accessibilityHelp",
+        "|",
+        "undo",
+        "redo",
+        "|",
+        "heading",
+        "|",
+        {
+          label: "Basic text styles",
+          icon: "bold",
+          items: [
+            "bold",
+            "italic",
+            "underline",
+            "strikethrough",
+            "subscript",
+            "superscript",
+          ],
+        },
+        {
+          label: "Font styles",
+          icon: "text",
+          items: ["fontSize", "fontColor", "fontBackgroundColor"],
+        },
+        "|",
+        {
+          label: "Indentation",
+          icon: "pilcrow",
+          items: ["outdent", "indent"],
+        },
+        "alignment",
+        "|",
+        {
+          label: "Embed content",
+          icon: "plus",
+          items: ["link", "imageInsert", "blockQuote", "insertTable"],
+        },
+        {
+          label: "Insert lists",
+          icon: "dragIndicator",
+          items: ["numberedList", "bulletedList", "todoList"],
+        },
+        "horizontalLine",
+        "specialCharacters",
+        "|",
+        "findAndReplace",
+        "selectAll",
+        "showBlocks",
+        "|",
+        "removeFormat",
+      ],
+      shouldNotGroupWhenFull: true,
+    },
+    language: "en",
+    heading: {
+      options: [
+        {
+          model: "paragraph",
+          title: "Paragraph",
+          class: "ck-heading_paragraph",
+        },
+        {
+          model: "heading2",
+          view: "h2",
+          title: "Heading 2",
+          class: "ck-heading_heading2",
+        },
+        {
+          model: "heading3",
+          view: "h3",
+          title: "Heading 3",
+          class: "ck-heading_heading3",
+        },
+        {
+          model: "heading4",
+          view: "h4",
+          title: "Heading 4",
+          class: "ck-heading_heading4",
+        },
+        {
+          model: "heading5",
+          view: "h5",
+          title: "Heading 5",
+          class: "ck-heading_heading5",
+        },
+      ],
+    },
+    image: {
+      toolbar: [
+        "toggleImageCaption",
+        "imageTextAlternative",
+        "|",
+        {
+          name: "image:inlineDropdown",
+          title: "Inline styles",
+          items: [
+            "imageStyle:alignLeft",
+            "imageStyle:inline",
+            "imageStyle:alignRight",
+          ],
+          defaultItem: "imageStyle:alignLeft",
+        },
+        {
+          name: "image:blockDropdown",
+          title: "Block styles",
+          items: [
+            "imageStyle:alignBlockLeft",
+            "imageStyle:alignCenter",
+            "imageStyle:alignBlockRight",
+          ],
+          defaultItem: "imageStyle:alignCenter",
+        },
+        "|",
+        "linkImage",
+        "resizeImage",
+      ],
+      insert: { type: "auto" },
+    },
+    link: { allowedProtocols: ["https?", "tel", "mailto"] },
+    list: { properties: { startIndex: true, reversed: true } },
+    table: {
+      contentToolbar: [
+        "toggleTableCaption",
+        "|",
+        "tableColumn",
+        "tableRow",
+        "mergeTableCells",
+        "|",
+        "tableCellProperties",
+        "tableProperties",
+      ],
+    },
+  };
 }
 
 export default Editor;
