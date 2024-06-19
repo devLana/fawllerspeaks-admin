a003faf5aac1103f8831cf8a05c2fcad46c248ec
ade_lana <lanadvictor@yahoo.co.uk>
2024-06-19 19:50:25 +0100

    [server]-refactor: improve getPostUrl util function

--- server/src/features/posts/utils/getPostUrl/getPostUrl.test.ts
+++ server/src/features/posts/utils/getPostUrl/getPostUrl.test.ts
@@ -8,14 +8,93 @@ test.each([
     "MY Blog POST SLuG",
     {
       slug: "my-blog-post-slug",
-      url: `${urls.siteUrl}/blog/my-blog-post-slug`,
+      href: `${urls.siteUrl}/blog/my-blog-post-slug`,
     },
   ],
   [
     "My-NEW#Blog_POST | SLug",
     {
-      slug: "my-new#blog-post-|-slug",
-      url: `${urls.siteUrl}/blog/my-new#blog-post-|-slug`,
+      slug: "my-new-blog-post-slug",
+      href: `${urls.siteUrl}/blog/my-new-blog-post-slug`,
+    },
+  ],
+  [
+    "A _ Crazy . .. Blog Post .Title # ## #1",
+    {
+      slug: "a-crazy-blog-post-title-1",
+      href: `${urls.siteUrl}/blog/a-crazy-blog-post-title-1`,
+    },
+  ],
+  [
+    "A Blog Post For Oranges, Peaches & Grapes",
+    {
+      slug: "a-blog-post-for-oranges-peaches-grapes",
+      href: `${urls.siteUrl}/blog/a-blog-post-for-oranges-peaches-grapes`,
+    },
+  ],
+  [
+    "New Shoes Now cost $12.85 More.",
+    {
+      slug: "new-shoes-now-cost-12-85-more",
+      href: `${urls.siteUrl}/blog/new-shoes-now-cost-12-85-more`,
+    },
+  ],
+  [
+    "@john_doe's Blog Post Title!",
+    {
+      slug: "john-does-blog-post-title",
+      href: `${urls.siteUrl}/blog/john-does-blog-post-title`,
+    },
+  ],
+  [
+    "A Tïtle With Nón Ãlphabet Charàcters",
+    {
+      slug: encodeURIComponent("a-tïtle-with-nón-ãlphabet-charàcters"),
+      href: `${urls.siteUrl}/blog/${encodeURIComponent(
+        "a-tïtle-with-nón-ãlphabet-charàcters"
+      )}`,
+    },
+  ],
+  [
+    'My Friend\'s Birthday. The Day John Doe Told Me "I Love You" 🥰 - Part 1',
+    {
+      slug: "my-friends-birthday-the-day-john-doe-told-me-i-love-you-part-1",
+      href: `${urls.siteUrl}/blog/my-friends-birthday-the-day-john-doe-told-me-i-love-you-part-1`,
+    },
+  ],
+  [
+    "These Guys Aren't Taller Than James's Brothers",
+    {
+      slug: "these-guys-are-not-taller-than-james-brothers",
+      href: `${urls.siteUrl}/blog/these-guys-are-not-taller-than-james-brothers`,
+    },
+  ],
+  [
+    "It's Time For Music My Friends Won't Listen To",
+    {
+      slug: "it-is-time-for-music-my-friends-will-not-listen-to",
+      href: `${urls.siteUrl}/blog/it-is-time-for-music-my-friends-will-not-listen-to`,
+    },
+  ],
+  [
+    "USERNAME JAMES123 SAID THEY'RE GOING OUT FOR LUNCH",
+    {
+      slug: "username-james123-said-they-are-going-out-for-lunch",
+      href: `${urls.siteUrl}/blog/username-james123-said-they-are-going-out-for-lunch`,
+    },
+  ],
+  [
+    "The Boys Said They've Tried Their Best. I'll keep Praying For Them",
+    {
+      slug: "the-boys-said-they-have-tried-their-best-i-will-keep-praying-for-them",
+      href: `${urls.siteUrl}/blog/the-boys-said-they-have-tried-their-best-i-will-keep-praying-for-them`,
+    },
+  ],
+  [
+    "Won't The Government Reduce Oil Prices?",
+    {
+      slug: "won-t-the-government-reduce-oil-prices",
+      href: `${urls.siteUrl}/blog/won-t-the-government-reduce-oil-prices`,
     },
   ],
 ])(
--- server/src/features/posts/utils/getPostUrl/index.ts
+++ server/src/features/posts/utils/getPostUrl/index.ts
@@ -1,17 +1,40 @@
+import { URL } from "node:url";
+
 import { urls } from "@utils/ClientUrls";
+import type { PostUrl } from "@resolverTypes";
 
-const getPostUrl = (title: string) => {
-  const slug = title
+const getPostUrl = (title: string): PostUrl => {
+  const regex = /it's|s's|'s|won't|n't|'\p{L}{1,5}|(?: *[^\p{L}\p{N}]+ *)+/gu;
+
+  const sluggedTitle = title
     .toLowerCase()
-    .split(/[\s_-]/g)
-    .reduce((result, word, index, arr) => {
-      const delimiter = index === arr.length - 1 ? "" : "-";
-      const str = `${word}${delimiter}`;
+    .replace(regex, (match, ...rest: [number, string]) => {
+      const [, titleStr] = rest;
 
-      return `${result}${str}`;
-    }, "");
+      if (match === "it's") return "it-is";
+      if (match === "s's" || match === "'s") return "s";
 
-  return { url: `${urls.siteUrl}/blog/${slug}`, slug };
+      if (match === "won't") {
+        return titleStr.startsWith(match) ? "won-t" : "will-not";
+      }
+
+      if (match === "n't") return "-not";
+
+      if (match.includes("'")) {
+        if (match.includes("'re")) return "-are";
+        if (match.includes("'ll")) return "-will";
+        if (match.includes("'ve")) return "-have";
+
+        return "";
+      }
+
+      return "-";
+    })
+    .replace(/(?:^-)|(?:-$)/g, "");
+
+  const { href, pathname } = new URL(sluggedTitle, `${urls.siteUrl}/blog/`);
+
+  return { href, slug: pathname.substring(6) };
 };
 
 export default getPostUrl;
