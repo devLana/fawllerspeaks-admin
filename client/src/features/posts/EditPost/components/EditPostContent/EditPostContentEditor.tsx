e8f133d998b5977e09ed8e8b3a609325d0ddb8b1
ade_lana <lanadvictor@yahoo.co.uk>
2025-06-09 04:03:32 +0100

    [client]-refactor(EditPost): a small fix for merging images into the save image urls in local storage

--- client/src/features/posts/EditPost/components/EditPostContent/EditPostContentEditor.tsx
+++ client/src/features/posts/EditPost/components/EditPostContent/EditPostContentEditor.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 
 import useDeletePostContentImages from "@hooks/useDeletePostContentImages";
 import CKEditorComponent from "@features/posts/components/CKEditorComponent";
-import { saveEditStoragePost as cb } from "@utils/posts/editStoragePost";
+import * as storage from "@utils/posts/editStoragePost";
 import type CustomEditor from "ckeditor5-custom-build";
 import type { EditPostContentEditorProps } from "types/posts/editPost";
 
 const EditPostContentEditor = (props: EditPostContentEditorProps) => {
   const savedImageUrls = React.useRef(new Set<string>());
-  const imgUrls = React.useRef(new Set<string>());
+  const imageUrls = React.useRef(new Set<string>());
   const timerId = React.useRef<number>();
 
   React.useEffect(() => {
@@ -22,6 +22,8 @@ const EditPostContentEditor = (props: EditPostContentEditorProps) => {
   const handleChange = (editorRef: CustomEditor) => {
     const root = editorRef.model.document.getRoot();
     const content = editorRef.getData().replace(/<p>(?:<br>)*&nbsp;<\/p>/g, "");
+    const post = storage.getEditStoragePost();
+    let imgUrls: string[] = [];
 
     if (root) {
       const range = editorRef.model.createRangeIn(root);
@@ -34,8 +36,9 @@ const EditPostContentEditor = (props: EditPostContentEditorProps) => {
           item.is("element", "imageInline")
         ) {
           const src = item.getAttribute("src");
+
           if (src && typeof src === "string") {
-            if (!savedImageUrls.current.has(src)) imgUrls.current.add(src);
+            if (!savedImageUrls.current.has(src)) imageUrls.current.add(src);
             currentImageUrls.add(src);
           }
         }
@@ -43,7 +46,7 @@ const EditPostContentEditor = (props: EditPostContentEditorProps) => {
 
       const removedImages = Array.from(savedImageUrls.current).filter(url => {
         if (!currentImageUrls.has(url)) {
-          imgUrls.current.delete(url);
+          imageUrls.current.delete(url);
           return true;
         }
 
@@ -55,11 +58,19 @@ const EditPostContentEditor = (props: EditPostContentEditorProps) => {
       savedImageUrls.current = currentImageUrls;
     }
 
+    if (post?.imgUrls && post.imgUrls.length > 0) {
+      const merged = new Set(post.imgUrls);
+      imageUrls.current.forEach(url => merged.add(url));
+      imgUrls = Array.from(merged);
+    } else {
+      imgUrls = Array.from(imageUrls.current);
+    }
+
     if (timerId.current) window.clearTimeout(timerId.current);
 
-    timerId.current = window.setTimeout(cb, 1500, {
+    timerId.current = window.setTimeout(storage.saveEditStoragePost, 1500, {
       content,
-      imgUrls: Array.from(imgUrls.current),
+      imgUrls,
     });
   };
 
