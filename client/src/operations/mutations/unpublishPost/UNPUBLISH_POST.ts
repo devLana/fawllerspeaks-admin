fca4a7b1190d4c5d8848ec52c94ef4788bb7f736
ade_lana <lanadvictor@yahoo.co.uk>
2025-08-29 02:08:31 +0100

    [client]-chore(UnpublishPost): selected minimal data for the operation response

--- client/src/operations/mutations/unpublishPost/UNPUBLISH_POST.ts
+++ client/src/operations/mutations/unpublishPost/UNPUBLISH_POST.ts
@@ -1,24 +1,26 @@
 import { gql, type TypedDocumentNode } from "@apollo/client";
 
-import { POST_FIELDS } from "@fragments/POST";
 import type { MutationUnpublishPostArgs as Args } from "@apiTypes";
 import type { UnpublishPostData as Data } from "types/posts/unpublishPost";
 
 type UnpublishPost = TypedDocumentNode<Data, Args>;
 
 export const UNPUBLISH_POST: UnpublishPost = gql`
-  ${POST_FIELDS}
   mutation UnpublishPost($postId: ID!) {
     unpublishPost(postId: $postId) {
       ... on PostIdValidationError {
-        postIdError
+        __typename
       }
       ... on BaseResponse {
         __typename
       }
       ... on SinglePost {
         post {
-          ...PostFields
+          id
+          url {
+            slug
+          }
+          status
         }
       }
     }
