0663a72fe1f4fe0c6606000cae20a6f76ea568dd
ade_lana <lanadvictor@yahoo.co.uk>
2025-10-29 05:35:27 +0100

    [server]-chore: noindex,nofollow meta tag on pages

--- client/src/layouts/components/Metadata.tsx
+++ client/src/layouts/components/Metadata.tsx
@@ -4,6 +4,7 @@ import type { MetaDataProps } from "types/layouts";
 const Metadata = ({ title, description }: MetaDataProps) => (
   <Head>
     <title>{title}</title>
+    <meta name="robots" content="noindex,nofollow" />
     {description && <meta name="description" content={description} />}
   </Head>
 );
