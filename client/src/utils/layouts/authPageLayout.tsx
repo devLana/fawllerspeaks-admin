322eec420deefeb0cba0cffea343457feba23617
ade_lana <lanadvictor@yahoo.co.uk>
2026-07-24 20:56:55 +0100

    [client]-chore: removed Typography from authPageLayout function

--- client/src/utils/layouts/authPageLayout.tsx
+++ client/src/utils/layouts/authPageLayout.tsx
@@ -1,26 +1,17 @@
 import type { NextPage } from "next";
 
-import Typography from "@mui/material/Typography";
-
 import AuthRootLayout from "@layouts/AuthRootLayout";
 import type { MetadataProps } from "@appTypes";
 
 const authPageLayout = <P extends object = object>(
   Page: NextPage<P>,
-  pageMetadata: MetadataProps & { pageTitle: string },
+  metadata: MetadataProps
 ) => {
-  const AuthPageLayout = (pageProps: P) => {
-    const { pageTitle, ...metadata } = pageMetadata;
-
-    return (
-      <AuthRootLayout {...metadata}>
-        <Typography variant="h1" id="page-title">
-          {pageTitle}
-        </Typography>
-        <Page {...pageProps} />
-      </AuthRootLayout>
-    );
-  };
+  const AuthPageLayout = (pageProps: P) => (
+    <AuthRootLayout {...metadata}>
+      <Page {...pageProps} />
+    </AuthRootLayout>
+  );
 
   return AuthPageLayout;
 };
